945d39e0903cb614beadfa855b13a69221565d13
pitGitHub2016 <gekko1990@gmail.com>
2021-08-18 10:33:22 +0300

    Add files via upload

--- PyEEG_Benchmark.py
+++ PyEEG_Benchmark.py
@@ -1,5 +1,6 @@
 from Slider import Slider as sl
 import scipy.io
+import itertools
 import numpy as np, investpy, time, pickle
 import pandas as pd
 from tqdm import tqdm
@@ -52,34 +53,57 @@ def RunWithMatlabData(mode):
         X_testSet = data[1]
         eig_trainingSet = data[2]
         eig_Simulation = data[3]
-        CI = data[4]
+        MeanSim = data[4]
+        eig_Simulation_Lower_Band = data[5]
+        eig_Simulation_Upper_Band = data[6]
+        CI_manifold = data[7]
         print("X_trainingSet.shape = ", X_trainingSet.shape)
         print("X_testSet.shape = ", X_testSet.shape)
         print("eig_trainingSet.shape = ", eig_trainingSet.shape)
         print("eig_Simulation.shape = ", eig_Simulation.shape)
-        print("CI.shape = ", CI.shape)
+        print("MeanSim.shape = ", MeanSim.shape)
+        print("eig_Simulation_Lower_Band.shape = ", eig_Simulation_Lower_Band.shape)
+        print("eig_Simulation_Upper_Band.shape = ", eig_Simulation_Upper_Band.shape)
+        print("CI_manifold.shape = ", CI_manifold.shape)
+
+        mse_eig_Simulation_standalone = (np.square(0 - X_testSet)).mean(axis=None)
+        print("mse_eig_Simulation_standalone = ", mse_eig_Simulation_standalone)
 
         for liftMethod in ["GH", "LP", "KR"]:
-            lift_out = Lift(liftMethod, X_trainingSet, X_testSet, eig_trainingSet, eig_Simulation)
-            print(liftMethod, ", ", lift_out[0])
-            print(liftMethod, ", lift_out[0].shape = ", lift_out[0].shape, ", mse = ", lift_out[1])
+            lift_out_eig_Simulation = Lift(liftMethod, X_trainingSet, X_testSet, eig_trainingSet, eig_Simulation)
+            mse_eig_Simulation = np.round(lift_out_eig_Simulation[1],4)
+            print(liftMethod, ", lift_out_eig_Simulation[0].shape = ", lift_out_eig_Simulation[0].shape, ", mse_eig_Simulation = ", mse_eig_Simulation)
 
-            lift_out_CI = Lift(liftMethod, X_trainingSet, X_testSet, eig_trainingSet, CI)
-            print(liftMethod, ", ", lift_out_CI[0])
-            print(liftMethod, ", lift_out_CI[0].shape = ", lift_out_CI[0].shape, ", mse = ", lift_out_CI[1])
+            lift_out_MeanSim = Lift(liftMethod, X_trainingSet, X_testSet, eig_trainingSet, MeanSim)
+            mse_MeanSim = np.round(lift_out_MeanSim[1],4)
+            print(liftMethod, ", ", lift_out_MeanSim[0], ", mse_MeanSim = ", mse_MeanSim)
+
+            lift_out_eig_Simulation_Lower_Band = Lift(liftMethod, X_trainingSet, X_testSet, eig_trainingSet, eig_Simulation_Lower_Band)
+            mse_eig_Simulation_Lower_Band = np.round(lift_out_eig_Simulation_Lower_Band[1],4)
+            print(liftMethod, ", ", lift_out_eig_Simulation_Lower_Band[0], ", mse_eig_Simulation_Lower_Band = ", mse_eig_Simulation_Lower_Band)
+
+            lift_out_eig_Simulation_Upper_Band = Lift(liftMethod, X_trainingSet, X_testSet, eig_trainingSet, eig_Simulation_Upper_Band)
+            mse_eig_Simulation_Upper_Band = np.round(lift_out_eig_Simulation_Upper_Band[1],4)
+            print(liftMethod, ", ", lift_out_eig_Simulation_Upper_Band[0], ", mse_eig_Simulation_Upper_Band = ", mse_eig_Simulation_Upper_Band)
+
+            lift_out_CI_manifold_Lower_Band = Lift(liftMethod, X_trainingSet, X_testSet, eig_trainingSet, CI_manifold[0,:].reshape(1,-1))
+            mse_CI_manifold_Lower_Band = np.round(lift_out_CI_manifold_Lower_Band[1],4)
+            print(liftMethod, ", ", lift_out_CI_manifold_Lower_Band[0], ", mse_CI_manifold_Lower_Band = ", mse_CI_manifold_Lower_Band)
+
+            lift_out_CI_manifold_Upper_Band = Lift(liftMethod, X_trainingSet, X_testSet, eig_trainingSet, CI_manifold[1,:].reshape(1,-1))
+            mse_CI_manifold_Upper_Band = np.round(lift_out_CI_manifold_Upper_Band[1],4)
+            print(liftMethod, ", ", lift_out_CI_manifold_Upper_Band[0], ", mse_CI_manifold_Upper_Band = ", mse_CI_manifold_Upper_Band)
 
     else:
         mat = scipy.io.loadmat(
             'D:\Dropbox\VM_Backup\RollingManifoldLearning\SmartGlobalAssetAllocation\MatlabCode_EqFree_DMAPs\FinEngineering_Application\Lifting_GG\\Dynamic_DM_Matlab_Data_TS.mat')
         data = mat['Dynamic_DM_Matlab_Data_TS']
 
-        liftMethod = "GH"
+        liftMethod = "LP"
 
         lift_out_Preds_list = []
-        lift_out_CI_Lower_Band_list = []
-        lift_out_CI_Upper_Band_list = []
         real_x_list = []
-        for i in range(data.shape[0]):
+        for i in range(10):#range(data.shape[0]):
             print(i, ", ", data[i].shape)
 
             rowData = data[i]
@@ -88,15 +112,28 @@ def RunWithMatlabData(mode):
             Preds = rowData[2]
             CI_Lower_Band = rowData[3]
             CI_Upper_Band = rowData[4]
+            varSet = [[] for varN in range(Preds.shape[1])]
+            for varN in range(Preds.shape[1]):
+                varSet[varN] = [Preds[0][varN], CI_Lower_Band[0][varN], CI_Upper_Band[0][varN]]
+            allStatsCombos = [[x[0], x[1]] for x in itertools.product(*varSet)]
             y_i = rowData[5]
-            lift_out_Preds = Lift(liftMethod, y_intv, y_i, obj_evecs, Preds)
-            lift_out_CI_Lower_Band = Lift(liftMethod, y_intv, y_i, obj_evecs, CI_Lower_Band)
-            lift_out_CI_Upper_Band = Lift(liftMethod, y_intv, y_i, obj_evecs, CI_Upper_Band)
-            if liftMethod == "GH":
-                lift_out_Preds_list.append(lift_out_Preds)
-                lift_out_CI_Lower_Band_list.append(lift_out_CI_Lower_Band)
-                lift_out_CI_Upper_Band_list.append(lift_out_CI_Upper_Band)
-                real_x_list.append(y_i[0])
+            real_x_list.append(y_i[0])
+
+            liftedCombos = [[] for varN in range(len(allStatsCombos))]
+            ## Process restricted Data of each combination (lift them back) for EACH stats combo
+            countCombo = 0
+            for subPred in allStatsCombos:
+                lift_out_Preds = Lift(liftMethod, y_intv, y_i, obj_evecs, np.array(subPred).reshape(1, -1))[0]
+
+                if liftMethod not in ["GH"]:
+                    lift_out_Preds = lift_out_Preds[0]
+                liftedCombos[countCombo].append(lift_out_Preds)
+                countCombo += 1
+
+            lift_out_Preds_list.append(liftedCombos)
+
+        print(len(lift_out_Preds_list))
+        time.sleep(3000)
 
         extrapolatedPsi_to_X_Preds = np.array(lift_out_Preds_list)
         extrapolatedPsi_to_X_CI_Lower_Band = np.array(lift_out_CI_Lower_Band_list)
